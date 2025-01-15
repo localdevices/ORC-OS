14ecdd5c1e53885f163b01835948dc07a654ba7d
hcwinsemius <h.c.winsemius@gmail.com>
2025-01-15 16:08:25 +0100

    view for raspi camera #1

--- dashboard/src/views/cameraAim.jsx
+++ dashboard/src/views/cameraAim.jsx
@@ -3,13 +3,40 @@ import React, {useState, useEffect} from 'react';
 import reactLogo from '/react.svg'
 import orcLogo from '/orc_favicon.svg'
 import api from "../api"
+import './cameraAim.css'
 
 const CameraAim = () => {
-  const [count, setCount] = useState(0)
   const [videoFeedUrl, setVideoFeedUrl] = useState("");
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [isToggledOn, setIsToggledOn] = useState(false); // Toggle state
 
+  // Function to handle the toggle state change
+  const handleToggle = async () => {
+
+    setIsLoading(true);
+    const newState = !isToggledOn; // Determine new state
+    setIsToggledOn(newState); // Update toggle state
+    try {
+      if (newState) {
+        // Call endpoint for "enabled" state
+        await api.post('/pivideo/start');
+        console.log("PiCamera enabled.");
+        const feedUrl = `${api.defaults.baseURL}/pivideo/stream`;
+        setVideoFeedUrl(feedUrl);
+      } else {
+        // Call endpoint for "disabled" state
+        await api.post('/pivideo/stop');
+        console.log("PiCamera disabled.");
+      }
+    } catch (error) {
+      console.error("Error or disabling PiCamera:", error);
+      setError('Failed to enable/disable PiCamera.');
+    } finally {
+      console.log("Setting load status to false")
+      setIsLoading(false);
+    }
+  };
   // submit form to display stream
   const handleFormSubmit = async (event) => {
     event.preventDefault();
@@ -103,6 +130,14 @@ const CameraAim = () => {
                 Submit
             </button>
         </form>
+      </div>
+      <div className='container'>
+          <div className='mb-3 mt-3'>Start Raspberry Pi camera
+            <div className="form-check form-switch">
+              <label className="form-label" htmlFor="picamSwitch" style={{ marginLeft: '0' }}></label>
+              <input style={{width: "40px", height: "20px", borderRadius: "15px"}} className="form-check-input" type="checkbox" role="switch" id="picamSwitch" onClick={handleToggle}/>
+            </div>
+          </div>
       </div>
     </>
   )
