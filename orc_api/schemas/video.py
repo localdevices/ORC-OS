ca8d45090695312f0e25d6c6771525889f82bdc1
hcwinsemius <h.c.winsemius@gmail.com>
2025-03-28 17:41:36 +0100

    video ready to run method #23

--- orc_api/schemas/video.py
+++ orc_api/schemas/video.py
@@ -7,8 +7,11 @@ from typing import Optional, Union
 
 from pydantic import BaseModel, ConfigDict, Field
 
+from orc_api import crud
 from orc_api import db as models
+from orc_api.database import get_session
 from orc_api.schemas.time_series import TimeSeriesBase, TimeSeriesResponse
+from orc_api.schemas.video_config import VideoConfigBase
 
 
 # Pydantic model for responses
@@ -16,7 +19,8 @@ class VideoBase(BaseModel):
     """Base schema for video."""
 
     timestamp: datetime = Field(description="Timestamp of the video.")
-    video_config: Optional[int] = Field(description="Video configuration ID.", default=None)
+    video_config_id: Optional[int] = Field(description="Video configuration ID.", default=None)
+    video_config: Optional[VideoConfigBase] = Field(description="Video configuration.", default=None)
 
 
 class VideoCreate(VideoBase):
@@ -39,6 +43,46 @@ class VideoResponse(VideoBase):
     site_id: Optional[int] = Field(default=None, description="ID of site to which video belongs on LiveORC server.")
 
     model_config = ConfigDict(from_attributes=True)
+
+    @property
+    def ready_to_run(self):
+        """Must be called by AP scheduler to check if video is ready to run."""
+        # if the video has already been run, or is already in process, also return False
+        if not self.status == models.VideoStatus.NEW:
+            return False
+        # check if all run components are available
+        return self.allowed_to_run
+
+    @property
+    def allowed_to_run(self):
+        """Check if prerequisites are met for running video."""
+        # if there is no video config, return False immediately
+        if self.video_config is None:
+            return False
+        if self.time_series:
+            if self.time_series.h:
+                return True
+        # more complicated case, if there is optical, a cross section must be present!
+        db = get_session()
+        water_level_settings = crud.water_level.get(db)
+        if water_level_settings is None:
+            optical = False
+        else:
+            optical = water_level_settings.optical
+        if not optical:
+            # we are not allowed to try optical water levels, so return False
+            return False
+        # we are allowed optical, so check if there is a cross section
+        return self.video_config.cross_section is not None
+
+    def run(self):
+        """Run video."""
+        if not self.allowed_to_run:
+            raise Exception("Cannot run video, prerequisites not met.")
+        # assemble all information
+
+        # run with separate instance
+        pass
 
     def get_thumbnail(self, base_path: str):
         """Get thumbnail file name."""
