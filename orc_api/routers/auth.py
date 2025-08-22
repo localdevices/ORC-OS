d23f86ea1ba5da704151c477f45a0d966df83c4b
hcwinsemius <h.c.winsemius@gmail.com>
2025-08-22 08:45:10 +0200

    set expiry to 15 minutes #58

--- orc_api/routers/auth.py
+++ orc_api/routers/auth.py
@@ -15,7 +15,7 @@ router: APIRouter = APIRouter(prefix="/auth", tags=["security"])
 def create_token():
     """Create a JWT token."""
     payload = {
-        "exp": datetime.now(UTC) + timedelta(seconds=15),  # Token expires in 1 hour
+        "exp": datetime.now(UTC) + timedelta(minutes=15),  # Token expires in 1 hour
         "sub": "user",  # Example subject claim
     }
     return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
