1c8dca058259465d3208c4f73d840c92b4fc489f
hcwinsemius <h.c.winsemius@gmail.com>
2025-08-22 13:24:53 +0200

    add confetti to clarify first use #58 fix #85

--- dashboard/src/views/login.jsx
+++ dashboard/src/views/login.jsx
@@ -1,7 +1,9 @@
 // Login page for access to dashboard and back end
 import { useState, useEffect } from "react";
+import { useWindowSize } from "react-use";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../auth/useAuth.jsx";
+import Confetti from 'react-confetti'
 import orcLogo from "/orc_favicon.svg";
 
 const Login = () => {
@@ -10,7 +12,7 @@ const Login = () => {
   const [error, setError] = useState("");
   const navigate = useNavigate();
   const { login, passwordAvailable, setNewPassword } = useAuth();
-
+  const { width, height } = useWindowSize();
   // upon entering the page, check if a password exists in the database
   useEffect(()  => {
     async function fetchPasswordAvailable() {
@@ -37,7 +39,9 @@ const Login = () => {
   };
 
   return (
-    <div className="spinner-container">
+    <div>
+    {!passAvailable && <Confetti width={width} height={height} />}
+    <div className="spinner-container" style={{overflow: "hidden"}}>
       <div>
         <a href="https://openrivercam.org" target="_blank">
           <img src={orcLogo} className="logo" alt="ORC logo" style={{"height": "300px"}} />
@@ -46,7 +50,9 @@ const Login = () => {
       <div>
         {passAvailable ? (
       <p>Please enter your password to proceed</p>) : (
-          <p>Congratulations! This is your first use of ORC-OS! Please set a password now.</p>)
+        <div>
+          <p>Congratulations! This is your first use of ORC-OS! Please set a password now.</p>
+        </div>)
         }
       </div>
       <form onSubmit={handleLogin}>
@@ -59,6 +65,7 @@ const Login = () => {
         <button className="btn" type="submit">Login</button>
         {error && <p style={{ color: "red" }}>{error}</p>} {/* Display login error */}
       </form>
+    </div>
     </div>
   );
 };
