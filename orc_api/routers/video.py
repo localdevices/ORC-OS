b69c2071415e053f88499743a53b5f45b6eb7286
hcwinsemius <h.c.winsemius@gmail.com>
2025-09-11 19:49:08 +0200

    removed aiofiles dependency #58

--- orc_api/routers/video.py
+++ orc_api/routers/video.py
@@ -7,7 +7,6 @@ from datetime import datetime
 from typing import List, Optional, Union
 from zipfile import ZIP_DEFLATED
 
-import aiofiles
 import cv2
 import zipstream
 from fastapi import (  # Requests holds the app
@@ -383,21 +382,13 @@ async def upload_video(
     rel_file_path = os.path.join("videos", timestamp.strftime("%Y%m%d"), str(video_instance.id), file.filename)
     abs_file_path = os.path.join(UPLOAD_DIRECTORY, rel_file_path)
 
-    # Save the file asynchronously
-    async with aiofiles.open(abs_file_path, "wb") as f:
-        while chunk := await file.read(1024 * 1024):  # Read in 1MB chunks
-            await f.write(chunk)
-    # Save the file using chunked reading/writing
-    # with open(abs_file_path, "wb") as f:
-    #     while True:
-    #         chunk = await file.read(1024 * 1024)  # Read in 1MB chunks
-    #         if not chunk:
-    #             break  # Stop when no more data is left
-    #         f.write(chunk)
-
-    #
-    # with open(abs_file_path, "wb") as f:
-    #     f.write(await file.read())
+    # Save the file in chunks
+    with open(abs_file_path, "wb") as f:
+        while True:
+            chunk = await file.read(1024 * 1024)  # Read in 1MB chunks
+            if not chunk:
+                break  # Stop when no more data is left
+            f.write(chunk)
 
     # now update the video instance
     video_instance.file = rel_file_path
