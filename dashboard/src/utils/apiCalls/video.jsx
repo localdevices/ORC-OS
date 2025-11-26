d77e651bc6306ce412fa7e48f1e517c902c74872
hcwinsemius <h.c.winsemius@gmail.com>
2025-11-26 09:45:03 +0100

    moved video api calls #137

--- dashboard/src/utils/apiCalls/video.jsx
+++ dashboard/src/utils/apiCalls/video.jsx
@@ -1,62 +1,4 @@
-import api from "../api/api.js";
-
-export const fitGcps = async (imgDims, gcps, setMessageInfo) => {
-  console.log(gcps);
-  try {
-    const response = await api.post('/control_points/fit_perspective', {
-      "gcps": gcps,
-      "height": imgDims.height,
-      "width": imgDims.width,
-    });
-    // Extract `src_est` and `dst_est` from API response
-    console.log(response.data);
-    const { error } = response.data;
-
-    const err_round = Math.round(error * 1000) / 1000;
-    if (err_round > 0.1) {
-      setMessageInfo('warning', `GCPs successfully fitted, but with a large average error: ${err_round} m.`);
-    }
-    setMessageInfo('success', `GCPs successfully fitted to image, average error: ${err_round} m.`);
-    // map the estimated points on the widgets for plotting
-    // updateWidgets();
-    return response.data;
-  } catch (error) {
-    setMessageInfo('error', 'Failed to send coordinates:' + error.response.data.detail);
-    // Optionally, handle errors (e.g., display an error message)
-    return;
-  }
-};
-
-export const get_bbox = async (cameraConfig, points) => {
-  try {
-    const response = await api.post(
-      "/camera_config/bounding_box",
-      {
-        "camera_config": cameraConfig,
-        "points": points,
-      }
-    )
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-export const orcVersion = async () => {
-  try {
-    const response = await api.get("/updates/check");
-    // console.log(response.data);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-export const startUpdate = async () => {
-    // start the update process
-    const response = await api.post("/updates/start");
-    return response.data;
-}
+import api from "../../api/api.js";
 
 export const get_videos_ids = async (api, selectedIds, setMessageInfo) => {
   try {
@@ -93,13 +35,13 @@ export const listVideoCount = async (api, start, stop, status, sync_status, firs
 ) => {
   try {
     const response = await api.get("/video/count/", {params: {
-      start: start,
-      stop: stop,
-      status: status,
-      sync_status: sync_status,
-      first: first,
-      count: count,
-    }});
+        start: start,
+        stop: stop,
+        status: status,
+        sync_status: sync_status,
+        first: first,
+        count: count,
+      }});
     return response.data;
   } catch (error) {
     console.error(error);
