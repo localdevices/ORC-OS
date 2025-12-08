0d1c272b2c5be4e78e91c04c068a9283504d9a97
hcwinsemius <h.c.winsemius@gmail.com>
2025-12-08 16:44:31 +0100

    unit test for lifespan #141

---
+++ tests/test_main/test_lifespan.py
@@ -0,0 +1,39 @@
+"""Test lifespan set up of main app."""
+
+import pytest
+
+from orc_api.main import app, lifespan
+from orc_api.utils.queue import PriorityThreadPoolExecutor
+
+
+@pytest.mark.asyncio
+async def test_lifespan_initializes_state(monkeypatch, mocker):
+    fake_scheduler = mocker.Mock()
+    mocker.patch("orc_api.main.BackgroundScheduler", return_value=fake_scheduler)
+    mocker.patch("orc_api.main.schedule_disk_maintenance", return_value=None)
+    mocker.patch("orc_api.main.schedule_video_checker", return_value=None)
+    mocker.patch("orc_api.main.schedule_water_level", return_value=fake_scheduler)
+
+    fake_session = mocker.Mock()
+    mocker.patch("orc_api.main.get_session", return_value=fake_session)
+    mocker.patch("orc_api.main.crud.video.get_list", return_value=[])
+
+    # make delayed_sync_videos not do any operations, and pass this in create_task
+    async def fake_delayed_sync_videos(app_arg, logger_arg):
+        return
+
+    async def run_nothing(coro):
+        # just await the coroutine for testing purposes and return None
+        await coro
+        return None
+
+    mocker.patch("orc_api.main.delayed_sync_videos", fake_delayed_sync_videos)
+    mocker.patch("orc_api.main.get_session", return_value=fake_session)
+    mocker.patch("orc_api.main.crud.video.get_list", return_value=[])
+
+    mocker.patch("orc_api.main.asyncio.create_task", side_effect=run_nothing)  # ensures awaiting
+
+    async with lifespan(app):
+        assert app.state.scheduler is fake_scheduler
+        assert isinstance(app.state.executor, PriorityThreadPoolExecutor)
+        assert isinstance(app.state.process_list, list)
