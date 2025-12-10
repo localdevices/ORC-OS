ae81d4f864b5fd0bcdf79e9e58a1cfeaddfd35c6
hcwinsemius <h.c.winsemius@gmail.com>
2025-12-10 17:17:05 +0100

    tests routers time series, cross section WIP #144

---
+++ tests/test_routers/test_time_series_routers.py
@@ -0,0 +1,69 @@
+from datetime import datetime, timedelta
+
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
+
+from orc_api import db as models
+from orc_api.database import get_db
+from orc_api.db import Base
+from orc_api.main import app
+from orc_api.utils import queue
+
+engine = create_engine("sqlite:///:memory:", connect_args={"check_same_thread": False}, poolclass=StaticPool)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db_override():
+    Base.metadata.create_all(bind=engine)
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        # Base.metadata.drop_all(bind=engine)
+        session.close()
+
+
+@pytest.fixture
+def auth_client():
+    app.dependency_overrides[get_db] = get_db_override
+    app.state.session = next(get_db_override())
+    app.state.executor = queue.PriorityThreadPoolExecutor(max_workers=1)  # ThreadPoolExecutor(max_workers=1)
+    client = TestClient(app)
+    # credentials = HTTPBasicCredentials(password="welcome123")
+    credentials = {"password": "welcome123"}
+    # first create the password
+    _ = client.post("/api/auth/set_password/", params=credentials)
+    response = client.post("/api/auth/login/", params=credentials)
+    assert response.status_code == 200
+    return TestClient(app, cookies=response.cookies)
+
+
+def test_get_patch_time_series(auth_client):
+    # add some videos
+    db_session = next(get_db_override())
+    ts1 = models.TimeSeries(timestamp=datetime.now(), h=20.0)
+    ts2 = models.TimeSeries(timestamp=datetime.now() + timedelta(hours=1), h=21.0)
+
+    db_session.add_all([ts1, ts2])
+    db_session.commit()
+    r = auth_client.get("/api/time_series/1/")
+    assert r.status_code == 200
+    r = auth_client.get("/api/time_series/2/")
+    assert r.status_code == 200
+    r = auth_client.get("/api/time_series/3/")
+    assert r.status_code == 404
+    # try patching time series
+    r = auth_client.patch("/api/time_series/1/", json={"h": 22.0})
+
+    assert r.status_code == 200
+    assert r.json()["h"] == 22.0
+    # also check in database if h was changed
+    rec = db_session.query(models.TimeSeries).get(1)
+    assert rec.h == 22.0
+    # remove after test
+    db_session.query(models.TimeSeries).delete()
+    db_session.commit()
+    db_session.flush()
